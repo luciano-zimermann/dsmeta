a26711c076cfcd81d4817beb314b65f42cb79c4a
Luciano Zimermann <lucianozn16@gmail.com>
2022-09-09 21:51:26 -0300

    API test

---
+++ backend/src/main/java/com/lucianozimermann/dsmeta/controllers/SaleController.java
@@ -0,0 +1,24 @@
+package com.lucianozimermann.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucianozimermann.dsmeta.entities.Sale;
+import com.lucianozimermann.dsmeta.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService service;
+	
+	@GetMapping
+	List<Sale> findSales() {
+		return service.findSales();
+	}
+}
---
+++ backend/src/main/java/com/lucianozimermann/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.lucianozimermann.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucianozimermann.dsmeta.entities.Sale;
+import com.lucianozimermann.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+}
